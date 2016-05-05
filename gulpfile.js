bb57ef7457707870846d50aa322ac5143445e3af
Joshua Azemoh <joshazemoh@gmail.com>
2016-05-05 22:51:44 +0100

    Define typescript task

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync'),
+    ts = require('gulp-typescript'),
 
     paths = {
       'sass': './src/sass/',
       'css': './build/css/',
-      'site': './build'
+      'scripts': './src/scripts/',
+      'js': './build/js/',
+      'site': './build/'
     };
 
 
@@ -20,11 +23,19 @@ gulp.task('sass', function () {
 });
 
 
-// Browser sync task to launch a server and auto-reload
-gulp.task('browser-sync', ['sass'], function () {
+// Browser sync task: to launch a server and auto-reload
+gulp.task('browser-sync', ['sass', 'scripts'], function () {
   browserSync({ server: {
       baseDir: paths.site
     }});
+});
+
+
+// Scripts task: Compile TypeScript files to js
+gulp.task('scripts', ['reload'], function () {
+  return gulp.src(paths.scripts + '*.ts')
+    .pipe(ts())
+    .pipe(gulp.dest(paths.js));
 });
 
 
@@ -34,10 +45,11 @@ gulp.task('reload', function () {
 });
 
 
-// Watch task: watch for file changes and trigger
-// appropriate task
+// Watch task: watch for file changes and
+// trigger appropriate task.
 gulp.task('watch', function () {
   gulp.watch(paths.sass + '**/*.scss', ['sass']); // Watch sass files
+  gulp.watch(paths.scripts + '**/*.ts', ['scripts']); // Watch .ts files
   gulp.watch(paths.site + '**/*.html', ['reload']); // Watch html files
 });
 
---
+++ src/scripts/person.ts
@@ -0,0 +1,15 @@
+class Person {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  setName(name: string) {
+    this.name = name;
+  }
+}
